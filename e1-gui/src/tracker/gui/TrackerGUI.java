e915ed02d42f48735c6c804ba40283456c9b630d
Alchemy Meister <jesusesma@gmail.com>
2015-10-19 17:25:47 +0200

    irene's lib

--- e1-gui/src/tracker/gui/TrackerGUI.java
+++ e1-gui/src/tracker/gui/TrackerGUI.java
@@ -126,30 +126,30 @@ public class TrackerGUI extends JFrame {
 		JPanel basicInfoPanel = new JPanel();
 		basicInfoPanel.setBackground(tabbedPanelColor);
 		tabbedPane.addTab("Basic info", null, basicInfoPanel, null);
-		basicInfoPanel.setLayout(new MigLayout("", "[][grow][]", "[20px:n,fill][20px:n,fill][][][][20px:n,fill][fill]"));
+		basicInfoPanel.setLayout(new MigLayout("", "[][grow][]", "[20px:n,grow][][][][20px:n,grow,fill]"));
 		
 		JLabel lblId = new JLabel("ID");
-		basicInfoPanel.add(lblId, "cell 0 2,alignx trailing");
+		basicInfoPanel.add(lblId, "cell 0 1,alignx trailing");
 		
 		textField = new JTextField();
-		basicInfoPanel.add(textField, "cell 1 2,growx");
+		basicInfoPanel.add(textField, "cell 1 1,growx");
 		textField.setColumns(10);
 		
 		JLabel lblIp = new JLabel("IP");
-		basicInfoPanel.add(lblIp, "cell 0 3,alignx trailing");
+		basicInfoPanel.add(lblIp, "cell 0 2,alignx trailing");
 		
 		textField_2 = new JTextField();
-		basicInfoPanel.add(textField_2, "cell 1 3,growx");
+		basicInfoPanel.add(textField_2, "cell 1 2,growx");
 		textField_2.setColumns(10);
 		
 		JButton startStopButton = new JButton("Provoke error");
-		basicInfoPanel.add(startStopButton, "cell 2 3");
+		basicInfoPanel.add(startStopButton, "cell 2 2");
 		
 		JLabel lblPort = new JLabel("Port");
-		basicInfoPanel.add(lblPort, "cell 0 4");
+		basicInfoPanel.add(lblPort, "cell 0 3");
 		
 		textField_1 = new JTextField();
-		basicInfoPanel.add(textField_1, "cell 1 4,growx");
+		basicInfoPanel.add(textField_1, "cell 1 3,growx");
 		textField_1.setColumns(10);
 		
 		JPanel trackerPanel = new JPanel();
