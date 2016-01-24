453eab1757adece6f56728446981a10e9f1186b1
7flying <diez.f.irene@gmail.com>
2016-01-24 22:54:04 +0100

    Add utilities to convert hex string to byte array
    -Tested on Sha1Test

--- e1-gui/src/common/utils/Utilities.java
+++ e1-gui/src/common/utils/Utilities.java
@@ -2,6 +2,9 @@ package common.utils;
 
 import java.util.regex.Pattern;
 
+/**
+ * This is real OOP.
+ */
 public class Utilities {
 	public static boolean isValidIP(String ip) {
 		String pattern = "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$";
@@ -49,5 +52,15 @@ public class Utilities {
 		    (byte)((bytes >>>  8) & 0xff),
 		    (byte)((bytes       ) & 0xff)
 		  };
-		}
+	}
+	
+	public static byte[] hexStringToByteArray(String s) {
+	    int len = s.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+	                             + Character.digit(s.charAt(i+1), 16));
+	    }
+	    return data;
+	}
 }
--- e1-gui/src/test/Sha1Test.java
+++ e1-gui/src/test/Sha1Test.java
@@ -17,5 +17,26 @@ public class Sha1Test {
 		for (byte i : hashBytes)
 			System.out.printf("0x%02X ", i);
 		System.out.println();
+		System.out.println("--");
+		byte [] stringBytes = new String(bytes).getBytes();
+		for (byte i : stringBytes)
+			System.out.printf("0x%02X ", i);
+		System.out.println();
+		System.out.println("--");
+		byte [] byteFromHex = hexStringToByteArray(bytes);
+		for (byte i : byteFromHex)
+			System.out.printf("0x%02X ", i);
+		System.out.println();
+		
+	}
+	
+	public static byte[] hexStringToByteArray(String s) {
+	    int len = s.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+	                             + Character.digit(s.charAt(i+1), 16));
+	    }
+	    return data;
 	}
 }
